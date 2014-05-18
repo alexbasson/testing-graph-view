d53ebdc05a42081f2f7ea42d28f7a0a8cb29a565
Alex Basson <alexbasson@gmail.com>
2014-05-17 20:18:18 -0400

    Initial Commit

---
+++ BPGraph/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BPGraph
+//
+//  Created by Alex on 5/17/14.
+//  Copyright (c) 2014 Conditionally Convergent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
